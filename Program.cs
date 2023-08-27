a334b4a629b91404a84f2f54438c328f8445e1a4
Theloopes <theloopesbr@gmail.com>
2023-08-27 01:26:18 -0300

    Ecxercicio incompleto

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(" --- Decomposição Decimal ---");
+Console.WriteLine("");
+Console.Write("Digite um número inteiro...: ");
+int numero = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine($"O número {numero} possui: ");
+Console.WriteLine($"         {} unidade(s)");
+Console.WriteLine($"         {} dezena(s)");
+Console.WriteLine($"         {} centena(s)");
