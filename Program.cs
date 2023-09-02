f46e372954b9786853499537ebf530e6c7b00c76
theloopes <e107ti@etec.sp.gov.br>
2023-09-01 21:27:45 -0300

    Exercicio resolvido

--- Program.cs
+++ Program.cs
@@ -2,7 +2,17 @@
 Console.WriteLine("");
 Console.Write("Digite um número inteiro...: ");
 int numero = Convert.ToInt32(Console.ReadLine()!);
+
+int resto = numero;
+
+int unidade = resto % 10;
+resto /= 10;
+int dezena = resto % 10;
+resto /= 10;
+int centena = resto % 10;
+
 Console.WriteLine($"O número {numero} possui: ");
-Console.WriteLine($"         {} unidade(s)");
-Console.WriteLine($"         {} dezena(s)");
-Console.WriteLine($"         {} centena(s)");
+Console.WriteLine($" {unidade} unidade(s)");
+Console.WriteLine($" {dezena} dezena(s)");
+Console.WriteLine($" {centena} centena(s)");
+
